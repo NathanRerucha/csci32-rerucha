a8aaa1594c4676593464ba5adb51cc917f22c5ae
Nathan Rerucha <nrerucha001@student.butte.edu>
2025-10-08 03:31:46 -0700

    Started typescript button

---
+++ apps/functioning-website/src/app/button/page.tsx
@@ -0,0 +1,6 @@
+import { button } from "ui";
+
+export default function ButtonPage() {
+  return <div className=""></div>
+  <Button></Button>
+}
--- apps/functioning-website/src/app/page.tsx
+++ apps/functioning-website/src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex h-dvh w-lvw justify-center items-center pointer-events-none hover:bg-linear-to-r/increasing from-indigo-500 to-teal-400">
       <h1 className="h-32 text-8xl text-shadow-lg bg-linear-to-r/increasing from-indigo-500 to-teal-400 text-transparent bg-clip-text pointer-events-auto hover:text-black transition duration-1500 ease-in">
-        "Creativity"
+        &quot;Creativity&quot;
       </h1>
     </main>
   )
