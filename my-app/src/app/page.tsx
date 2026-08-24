c972c3463e182d3ad8fc07141c185fc86ae76fe5
Nathan Rerucha <nrerucha001@student.butte.edu>
2026-08-24 04:41:07 -0700

    Editor and NextJS page set up

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <main className="">
+        <div className="bg-white p-10 rounded-xl">
+          <h1 className="text-5xl italic ">I Forgot Everything</h1>
+          <h2 className="text-2xl bold mt-20">I hate centering things</h2>
+        </div>
+      </main>
+    </div>
+  )
+}
